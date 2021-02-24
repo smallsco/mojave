b8146523105e5e8677c5fc4ee3edb3e4175d8b5d
Nettogrof <carl.lajeunesse@gmail.com>
2021-02-24 10:22:21 -0500

    Draw snake body curve in corners

--- modules/Board.lua
+++ modules/Board.lua
@@ -304,13 +304,60 @@ function Board:drawRaw( state, draw_grid )
                     sa = ( ( ( ( #snake.body - i ) * 150 ) / #snake.body ) + 105 ) / 255
                 end
                 love.graphics.setColor(sr, sg, sb, sa)
-                love.graphics.rectangle(
+
+                --Locate the previous and after body part position
+                local px, py, ax ,ay
+                px = snake.body[i-1].x - snake.body[i].x
+                py = snake.body[i-1].y - snake.body[i].y
+                ax = snake.body[i].x - snake.body[i+1].x
+                ay = snake.body[i].y - snake.body[i+1].y
+                
+                if px == ax and py == ay  then
+                    -- Draw rectangle if the snake body is straight 
+                    love.graphics.rectangle(
                     "fill",
                     1 + (snake.body[i].x * self.tileWidth) + ((self.tileWidth - self.cellWidth) / 2),
                     1 + ((self.numTilesY - 1 - snake.body[i].y) * self.tileHeight) + ((self.tileHeight - self.cellHeight) / 2),
                     self.cellWidth,
                     self.cellHeight
-                )
+                    )
+                else
+                    -- Draw a corner if the snake body 'turn'
+                    local centerx, centery
+                    
+                    if (py < 0 and ax < 0) or (ay > 0 and px > 0 )   then                        
+                        centerx = 1 + (snake.body[i].x * self.tileWidth) + ((self.tileWidth - self.cellWidth) / 2) + self.cellWidth
+                        centery = 1 + ((self.numTilesY - 1 - snake.body[i].y) * self.tileHeight) + ((self.tileHeight - self.cellHeight) / 2)  + self.cellHeight
+                    elseif (py > 0 and ax > 0 ) or (ay < 0 and px < 0 )   then 
+                        centerx = 1 + (snake.body[i].x * self.tileWidth) + ((self.tileWidth - self.cellWidth) / 2) 
+                        centery = 1 + ((self.numTilesY - 1 - snake.body[i].y) * self.tileHeight) + ((self.tileHeight - self.cellHeight) / 2)  
+                    elseif (py < 0 and ax > 0 ) or (ay > 0 and px < 0 )  then 
+                        centerx = 1 + (snake.body[i].x * self.tileWidth) + ((self.tileWidth - self.cellWidth) / 2) 
+                        centery = 1 + ((self.numTilesY - 1 - snake.body[i].y) * self.tileHeight) + ((self.tileHeight - self.cellHeight) / 2)  + self.cellHeight
+                    elseif (py > 0 and ax < 0 )  or (ay < 0 and px > 0 )  then    
+                        centerx = 1 + (snake.body[i].x * self.tileWidth) + ((self.tileWidth - self.cellWidth) / 2) + self.cellWidth
+                        centery = 1 + ((self.numTilesY - 1 - snake.body[i].y) * self.tileHeight) + ((self.tileHeight - self.cellHeight) / 2)
+                    end
+
+                    -- limit the drawing to the current cell
+                    love.graphics.setScissor(
+                        1 + (snake.body[i].x * self.tileWidth) + ((self.tileWidth - self.cellWidth) / 2),
+                        1 + ((self.numTilesY - 1 - snake.body[i].y) * self.tileHeight) + ((self.tileHeight - self.cellHeight) / 2),
+                        self.cellWidth+1,
+                        self.cellHeight+1    
+                    )
+                    love.graphics.ellipse(
+                        "fill",
+                        centerx,
+                        centery,
+                        self.cellWidth,
+                        self.cellHeight,
+                        50
+                    )
+                    love.graphics.setScissor()
+                end
+           
+
                 love.graphics.setColor(1, 1, 1, 1)
             end
         end
